758610f48d74125e214a423624c4d26a066e1a53
AntonisKl <antonisklironomos@gmail.com>
2019-03-25 02:20:54 +0200

    Added more functionality to create_infiles.sh bash script: it now creates files and folders in a round-robin manner

--- create_infiles.sh
+++ create_infiles.sh
@@ -4,52 +4,109 @@ if [[ "$#" -ne 4 ]]; then
     echo "Illegal number of parameters";  exit 1
 fi
 
-dirName = $0
-filesNum = $1
-dirsNum = $2
-levelsNum = $3
+dirName=$1
+filesNum=$2
+dirsNum=$3
+levelsNum=$4
 
 args=("$@")
 
 integerRegex='^[0-9]+$'
 
-for arg in "${args[@]}" do
-  if ! [[ $arg == $0 ]]
-    continue
-  fi
+for arg in "${args[@]}"
+do
+    if [[ $arg == $1 ]] || [[ $arg == $0 ]]; then
+        continue
+    fi
 
-  if ! [[ $arg =~ $integerRegex ]]; then
-     echo "Argument $arg is not an integer"; exit 1
-  fi
+    if ! [[ $arg =~ $integerRegex ]]; then
+        echo "Argument $arg is not an integer"; exit 1
+    fi
 done
 
 # create directory if it does not exist
 mkdir -p $dirName
 
-dirNames = ()
-dirFullNames = ()
-fileNames = ()
+dirNames=()
+dirFullNames=()
+fileNames=()
 
-for ((i=1; i<=$filesNum; i++)); do
-  fileNames[i] = head /dev/urandom | tr -dc A-Za-z0-9 | head -c (shuf -i 1-8 -n 1) ; echo ''
+for ((i=1; i<=$filesNum; i++));
+do
+    charsNum=$(shuf -i 1-8 -n 1)
+    fileNames[i]=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c $charsNum ; echo '')
+    # echo "${args[$i]}"
 done
 
-levelsNumTemp = 0
-for ((i=1; i<=$dirsNum; i++)); do
-   dirNames[i] = head /dev/urandom | tr -dc A-Za-z0-9 | head -c (shuf -i 1-8 -n 1) ; echo ''
+filesNumPerDir=$(( $filesNum / $dirsNum ))
+if (( $filesNumPerDir == 0 )); then
+    filesNumPerDir=1
+fi
 
-   curDirName = ${dirNames[i]}
-   for ((j=0; j<=$levelsNumTemp; j++)); do
-     curDirName = "${dirNames[i - j]}/$curDirName"
-   done
+echo "filesNumPerDir=$filesNumPerDir"
 
-   mkdir -p $curDirName
+levelsNumTemp=0
+fileNamesIndex=1
+for ((i=1; i<=$dirsNum; i++));
+do
+    charsNum=$(shuf -i 1-8 -n 1)
+    dirNames[i]=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c $charsNum ; echo '')
 
-   dirFullNames[i] = $curDirName
+    curDirName=${dirNames[i]}
+    for ((j=1; j<=$levelsNumTemp; j++));
+    do
+        curDirName="${dirNames[i - j]}/$curDirName"
+    done
 
-   ((levelsNumTemp ++))
+    curDirName="$dirName/$curDirName"
+    echo "$curDirName"
 
-   if [[ $levelsNumTemp == $levelsNum ]]
-    levelsNumTemp = 0
-   fi
+    mkdir -p $curDirName
+
+    for ((j=0; j<$filesNumPerDir; j++));
+    do
+        if (( $fileNamesIndex > $filesNum )); then
+            break
+        fi
+
+        touch "$curDirName/${fileNames[$fileNamesIndex]}"
+
+        ((fileNamesIndex ++))
+    done
+
+    dirFullNames[i]=$curDirName
+
+    ((levelsNumTemp ++))
+
+    if (( $levelsNumTemp == $levelsNum )); then
+        levelsNumTemp=0
+    fi
+done
+
+echo "filesIndex = $fileNamesIndex"
+
+# create remaining files
+while (( $fileNamesIndex <= $filesNum ));
+do
+    for dirFullName in "${dirFullNames[@]}"
+    do
+        for ((j=0; j<$filesNumPerDir; j++));
+        do
+            if (( $fileNamesIndex > $filesNum )); then
+                break
+            fi
+
+            touch "$dirFullName/${fileNames[$fileNamesIndex]}"
+
+            ((fileNamesIndex ++))
+        done
+
+        if (( $fileNamesIndex > $filesNum )); then
+            break
+        fi
+    done
+
+    if ((  $fileNamesIndex > $filesNum )); then
+        break
+    fi
 done
